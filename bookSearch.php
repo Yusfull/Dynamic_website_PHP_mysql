44bb423497afa0984b2ba231a496350df18761c2
Yusfull <profqolo@gmail.com>
2016-09-27 11:28:19 +0200

    dada

--- bookSearch.php
+++ bookSearch.php
@@ -6,7 +6,7 @@
    <body>
        <h3>Book Search Results<h3>
        <hr>
-       <?php
+<?php
          $searchTitle = trim($_POST['searchTitle']);
          $searchAuthor= trim($_POST['searchTitle']);
 
@@ -14,6 +14,18 @@
               printf("You must specify either a title or an author");
               exit();
          }
-       ?>
+try{
+    $db = new PDO("mysql:host=localhost;dbname=PHPFundamentals","root","root");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+}catch(PDOException $e){
+printf("We had a problem: %s\n", $e->getMessage());
+}
+
+$query = "select * from books";
+if($searchAuthor && ! $searchTitle){
+    $query = $query. " where title like '% " . $searchTitle. "%'";
+}
+?>
    </body>
 </html>
